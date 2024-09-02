f6605f15468b80479039f3df32373dc7522cbb08
MrFantomas <103043261+MrFantomas@users.noreply.github.com>
2024-09-02 12:52:52 +0300

    Add files via upload

--- src4/script.js
+++ src4/script.js
@@ -8,7 +8,7 @@ document.getElementById("checkMeButton").addEventListener("click", function() {
     } else if (clickCount === 20) {
         alert("Я тебя предупреждал!");
     } else if (clickCount === 25) {
-        alert("F12");
+        alert("kotKyzma");
     } else if (clickCount === 40) {
         document.getElementById("messageContainer").classList.remove("hidden");
     }
